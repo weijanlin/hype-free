2c8c722767032cb14d4b180aa8e4ff2167bc84f0
dify.ltd <dify.ltd@9bb63aa9-da4f-0410-94c6-0daccae7f317>
2010-03-23 12:14:20 +0000

    Added demo with DateFormat / ThreadLocal

---
+++ java-perfopt-201003/src/com/blogspot/hypefree/javaperfopt201003/TestDateTimeFormatter.java
@@ -0,0 +1,51 @@
+package com.blogspot.hypefree.javaperfopt201003;
+
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.CountDownLatch;
+
+public class TestDateTimeFormatter {
+	private final static int THREADS = 4;
+	private final static int ITERATIONS = 100000;
+	private final static String DATE_FORMAT = "yyyy-MM-dd HH:mm:ss.SSS z";
+	
+	private final static DateFormat staticDf = new SimpleDateFormat(DATE_FORMAT);
+	
+	private final static ThreadLocal<DateFormat> dfSource = new ThreadLocal<DateFormat>() {
+		protected synchronized DateFormat initialValue() {
+			return new SimpleDateFormat(DATE_FORMAT);
+		}
+	};
+	
+	public static void main(String[] args) throws Exception {
+		final CountDownLatch countDown = new CountDownLatch(THREADS);
+		final Date now = new Date();
+		
+		final Runnable r = new Runnable() {
+			@Override
+			public void run() {
+				try {
+					DateFormat df;
+					long started = System.currentTimeMillis();
+					for (int i = 0; i < ITERATIONS; ++i) {
+//						df = new SimpleDateFormat(DATE_FORMAT);
+						df = staticDf;
+//						df = dfSource.get();
+						synchronized (staticDf) {
+							df.parse(df.format(now));
+						}						
+					}
+					System.out.println("Time: " + (System.currentTimeMillis() - started) + " ms.");
+				}
+				catch (Exception e) {
+					e.printStackTrace();
+				}
+				countDown.countDown();
+			}
+		};
+		
+		for (int i = 0; i < THREADS; ++i) { new Thread(r).start(); }
+				
+		countDown.await();		
+	}
+}
