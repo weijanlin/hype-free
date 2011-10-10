7f5a92a62cdf54f9f2fa84793b011391ed91334e
dify.ltd@gmail.com <dify.ltd@gmail.com@9bb63aa9-da4f-0410-94c6-0daccae7f317>
2011-10-10 19:36:16 +0000

    Example for Maven-Ivy integration

---
+++ trunk/java-maven-ivy/src/test/java/org/transylvania/jug/espresso/shots/d20111010/FooTest.java
@@ -0,0 +1,13 @@
+package org.transylvania.jug.espresso.shots.d20111010;
+
+import org.jaudiotagger.logging.Hex;
+import org.junit.*;
+import net.sourceforge.groboutils.autodoc.v1.AutoDoc;
+
+public class FooTest {
+	@Test
+	public void testFoo() {
+		new AutoDoc(getClass());
+		new Hex();
+	}
+}
